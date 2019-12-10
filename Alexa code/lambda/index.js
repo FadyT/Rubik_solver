4b3400595306f67bad0e7816f92dc239cfde8054
piratemrs <piratemrs@gmail.com>
2019-12-10 16:39:07 +0200

    Rubik Solver v1

---
+++ Alexa code/lambda/index.js
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * You may not use this file except in compliance with the terms and conditions 
+ * set forth in the accompanying LICENSE.TXT file.
+ *
+ * THESE MATERIALS ARE PROVIDED ON AN "AS IS" BASIS. AMAZON SPECIFICALLY DISCLAIMS, WITH 
+ * RESPECT TO THESE MATERIALS, ALL WARRANTIES, EXPRESS, IMPLIED, OR STATUTORY, INCLUDING 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
+*/
+
+// This skill sample demonstrates how to send directives and receive events from an Echo connected gadget.
+// This skill uses the Alexa Skills Kit SDK (v2). Please visit https://alexa.design/cookbook for additional
+// examples on implementing slots, dialog management, session persistence, api calls, and more.
+
+const Alexa = require('ask-sdk-core');
+const Util = require('./util');
+const Common = require('./common');
+
+// The namespace of the custom directive to be sent by this skill
+const NAMESPACE = 'Custom.Mindstorms.Gadget';
+
+// The name of the custom directive to be sent this skill
+const NAME_CONTROL = 'control';
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle: async function(handlerInput) {
+
+        const request = handlerInput.requestEnvelope;
+        const { apiEndpoint, apiAccessToken } = request.context.System;
+        const apiResponse = await Util.getConnectedEndpoints(apiEndpoint, apiAccessToken);
+        if ((apiResponse.endpoints || []).length === 0) {
+            return handlerInput.responseBuilder
+            .speak(`I couldn't find an EV3 Brick connected to this Echo device. Please check to make sure your EV3 Brick is connected, and try again.`)
+            .getResponse();
+        }
+
+        // Store the gadget endpointId to be used in this skill session
+        const endpointId = apiResponse.endpoints[0].endpointId || [];
+        Util.putSessionAttribute(handlerInput, 'endpointId', endpointId);
+
+        // Set skill duration to 5 minutes (ten 30-seconds interval)
+        Util.putSessionAttribute(handlerInput, 'duration', 10);
+
+        // Set the token to track the event handler
+        const token = handlerInput.requestEnvelope.request.requestId;
+        Util.putSessionAttribute(handlerInput, 'token', token);
+
+        let speechOutput = "Welcome to rubik solver";
+        return handlerInput.responseBuilder
+            .speak(speechOutput )
+            .addDirective(Util.buildStartEventHandler(token,60000, {}))
+            .getResponse();
+    }
+};
+
+const SolveIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'SolveIntent';
+    },
+    handle: function (handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const endpointId = attributesManager.getSessionAttributes().endpointId || [];
+
+        // Construct the directive with the payload containing the solve command
+        const directive = Util.build(endpointId, NAMESPACE, NAME_CONTROL,
+            {
+                type: 'solve'        
+            });
+
+        return handlerInput.responseBuilder
+            .speak("Solving the rubik's Cube")
+            .addDirective(directive)
+            .getResponse();
+    }
+};
+
+
+
+const ExpiredRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'CustomInterfaceController.Expired'
+    },
+    handle(handlerInput) {
+        console.log("== Custom Event Expiration Input ==");
+
+        // Set the token to track the event handler
+        const token = handlerInput.requestEnvelope.request.requestId;
+        Util.putSessionAttribute(handlerInput, 'token', token);
+
+        const attributesManager = handlerInput.attributesManager;
+        let duration = attributesManager.getSessionAttributes().duration || 0;
+        if (duration > 0) {
+            Util.putSessionAttribute(handlerInput, 'duration', --duration);
+
+            // Extends skill session
+          
+        }
+        else {
+            // End skill session
+            return handlerInput.responseBuilder
+                .speak("Skill duration expired. Goodbye.")
+                .withShouldEndSession(true)
+                .getResponse();
+        }
+    }
+};
+
+// The SkillBuilder acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        SolveIntentHandler,
+        ExpiredRequestHandler,
+        Common.HelpIntentHandler,
+        Common.CancelAndStopIntentHandler,
+        Common.SessionEndedRequestHandler,
+        Common.IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+    )
+    .addRequestInterceptors(Common.RequestInterceptor)
+    .addErrorHandlers(
+        Common.ErrorHandler,
+    )
+    .lambda();
